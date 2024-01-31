c3e690c8239f9c7a353d30b556996647ee47aec6
MIkhail Vishniakov <grabilenko.yana@mail.ru>
2024-01-31 23:32:54 +0300

    Создал файл с заданиями

---
+++ рекурсии/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace рекурсии
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1: Задайте значения M и N.Напишите программу, которая выведет все натуральные числа в промежутке
+            //от M до N. Использовать рекурсию, не использовать циклы.
+            int m, n;
+            m = 10; n = 40;
+            RecNatural(m, n);
+            //Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+            //Задача 3: Задайте произвольный массив.Выведете его элементы, начиная с конца.Использовать рекурсию, не использовать циклы.
+        }
+        static int RecNatural(int a,int b)
+        {
+
+        }
+    }
+}
